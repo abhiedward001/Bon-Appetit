883a97b7d63a3854a214e8ead2ef0735894623d7
abhiedward001 <iam001abhi@gmail.com>
2021-11-03 22:59:02 +0530

    check

--- src/components/layout/Cartbutton.js
+++ src/components/layout/Cartbutton.js
@@ -5,24 +5,31 @@ import Cartcontext from '../../store/Cart-context';
 
 const Cartbutton = (props) => {
     const cartctx = useContext(Cartcontext);
+    
+    // will return total qty of items array
     const numberofcartitems = cartctx.items.reduce((CurNum, item) => {
         return CurNum + item.qty;
     }, 0);
-    const { items } = cartctx;
-    const [btnBump, setBtnBump] = useState(false);
+
+    const { items } = cartctx; //destructuring object
+    const [btnBump, setBtnBump] = useState(false); //state used for button animation
+
     const btnclass = `${classes.button} ${btnBump ? classes.bump : ''}`
+
     useEffect(() => {
         if (items.length === 0) {
             return;
         }
         setBtnBump(true);
+        //used timer to make condition false as after re-rendering we need to add bump class again
        const timer= setTimeout(() => {
             setBtnBump(false);
+            clearTimeout(timer)
         }, 300);
-        return()=>{
-            clearTimeout(timer);
-        };
+  
+       
     }, [items]);
+
     return (
         <>
             <button className={btnclass} onClick={props.onclk}>
